44f406121548f2bba7751758f19fb87987d63626
PsueDNymph <wynterjapp@outlook.com>
2024-06-18 11:08:13 -0500

    Add comments

--- entity.py
+++ entity.py
@@ -1,12 +1,13 @@
 class Entity:
-
-    def __init__(self):
-        self.attributes = {}  
-        
-    def __init__(self, attributeList):
+    #   Parameterized Constructor
+    #       creates empty attribute dictionary and uses the parameter attributeList 
+    #       to update teh List by calling addAttribute(attributeList)
+    def __init__(self, attributeList=()):
         self.attributes = {}  
         self.addAttribute(attributeList)
-            
+    #   addAttributes
+    #       updates the attribute dictionary using the attribute dictionary newAttributs
+    #       it was given
     def addAttributes(self, newAttributes):
         self.attributes.update(newAttributes)
 
