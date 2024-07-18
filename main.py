923c15a9709579cdaa80d766921177990e27df65
Victoria Lee <vlee@berkeley.edu>
2024-07-18 07:26:12 -0700

    RandomBuild1

--- main.py
+++ main.py
@@ -31,14 +31,15 @@ def build_grid(user_id, environment_id, permission_id, rows, columns, policy):
             grid.append([])
             for col in range(int(columns)):
                 object_id = "Grid" + str(row) + "x" + str(col)
+                print(policy.get_permission('nonEntry'))
                 if(policy.get_pa_relation().get_entries(policy.get_permission('nonEntry') ,object_id) != None):
                     nonEntry = check_permission(user_id,object_id, environment_id, 'nonEntry', policy,row,col)
                     if nonEntry:
                         grid[row].append(0)
                         continue
                 if(policy.get_pa_relation().get_entries(policy.get_permission(permission_id),object_id)!= None):
-                    print("activates")
                     grid[row].append(check_permission(user_id, object_id, environment_id, permission_id, policy,row,col))
+                print("NOT HERE")
             
 def execute_command(command):
     global policy
@@ -51,7 +52,7 @@ def execute_command(command):
             #print(command_parts[1])
             policy = ABACPolicyLoader.load_abac_policy(command_parts[1])
         case "build-grid":
-            print(command_parts[3])
+            #print(command_parts[3])
             rowsAndColums = command_parts[4].strip()[1:-1]
             rowsAndColums = rowsAndColums.split(",")
             row = int(rowsAndColums[0])
