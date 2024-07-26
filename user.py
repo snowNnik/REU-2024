d36faded6cec065fb0fa52c0ea3034387d25f9c6
snowNnik <99065585+snowNnik@users.noreply.github.com>
2024-07-26 10:14:26 -0700

    Delete user.py

--- user.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from Entity import *
-
-
-class User(Entity):
-    def __init__(self, name, assigned_attributes=None):
-        self.name = name
-        self.assigned_attributes = assigned_attributes or []
